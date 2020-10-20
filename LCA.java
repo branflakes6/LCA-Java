f441257777666d1aa8b95fe2a10965e00d68bc87
Brian Farrell <farrelb6@tcd.ie>
2020-10-20 20:51:40 +0100

    Update LCA.java
    Made Changes in order to add tests

--- LCA.java
+++ LCA.java
@@ -11,43 +11,31 @@ public class LCA {
             this.val = val;
             this.children = children;
         }
-    }
-
-    public static void main(String[] args)
-    {
-        Node<Integer, Integer> node6 = new Node<>(6,6,null);
-        Node<Integer, Integer> node5 = new Node<>(5,5,null);
-        Node<Integer, Integer> node4 = new Node<>(4,4,null);
-        Node<Integer, Integer> node3 = new Node<>(3,3, null);
-        Node<Integer, Integer> node2 = new Node<>(2,2, new Node[]{node5, node6});
-        Node<Integer, Integer> node1 = new Node<>(1,1, new Node[]{node3, node4});
-        Node<Integer, Integer> root = new Node<>(0, 0, new Node[]{node1, node2});
-        Node<Integer, Integer> LCA = findLCA(root, node5, node6);
-        System.out.println("The LCA of node node 5 and node 6 is "+ LCA.key);
-    }
-    public static Node findLCA (Node root, Node a, Node b) {
-        if (root == null || root.key.equals(a.key) || root.key.equals(b.key))
-        {
-            return root;
-        }
-        Node child = null;
-        int count = 0;
-        if (root.children != null)
-        {
-            for (int i = 0; i < root.children.length; i++)
-            {
-                Node temp = findLCA(root.children[i], a, b);
-                if (temp != null)
-                {
-                    count++;
-                    child = temp;
-                }
-                if (count >= 2)
-                {
+        public static Node findLCA (Node root, Node a, Node b) {
+            try {
+                if (root == null || root.key.equals(a.key) || root.key.equals(b.key)) {
                     return root;
                 }
+                Node child = null;
+                int count = 0;
+                if (root.children != null) {
+                    for (int i = 0; i < root.children.length; i++) {
+                        Node temp = findLCA(root.children[i], a, b);
+                        if (temp != null) {
+                            count++;
+                            child = temp;
+                        }
+                        if (count >= 2) {
+                            return root;
+                        }
+                    }
+                }
+                return child;
+            } catch (Exception e) {
+                System.out.println("Cannot find to LCA as either Root, a or b is Null");
+                return null;
             }
         }
-           return child;
     }
 }
+
